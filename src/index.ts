33a5bd21939ab0fa728fc9569a0ad7b275174eb3
coco <mrkokipro@icloud.com>
2020-07-12 22:45:33 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import * as Dotenv from "dotenv"
+import * as path from 'path'
+import { Sequelize } from "sequelize"
+
+Dotenv.config({path: path.resolve(__dirname, '../.env.local')})
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+})
+
+init()
+
+console.log(process.env.DB_PASSWORD)
+
+async function init() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
